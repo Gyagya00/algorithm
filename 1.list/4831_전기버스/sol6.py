0374547412368349cfa1c873409cddd573ae15e8
Gaeun Roh <shrkdms3389@naver.com>
2021-03-02 09:21:11 +0900

    im끝git add .

--- 1.list/4831_전기버스/sol6.py
+++ 1.list/4831_전기버스/sol6.py
@@ -16,28 +16,30 @@ for tc in range(1, T+1):
     fuel = K
     # 충전횟수
     cnt = 0
+    # 정류장 인덱스
+    i = 0
 
     while now < N:
-        for i in range(M):
-            # 정류장에 도착할때마다 충전가능
-            if now == bus_fuel[i]:
-                # 가장 멀리있는 다음정류장까지 갈 수 있다면
-                for j in range(M-1, i-1, -1):
-                    if bus_fuel[j] - now <= fuel:
-                        fuel -= bus_fuel[j] - now
-                        now = bus_fuel[j]
-                        fuel += K
-                        cnt += 1
-                        break
-                # else:
-                #     now = bus_fuel[i]
-                #     fuel += K
-                #     cnt += 1
+        # 정류장에 도착할때마다 충전가능
+        if i < M and now == bus_fuel[i]:
+            # 가장 멀리있는 다음정류장까지 갈 수 있다면
+            for j in range(M-1, i-1, -1):
+                if bus_fuel[j] - now <= fuel:
+                    fuel -= bus_fuel[j] - now
+                    now = bus_fuel[j]
+                    i = j + 1
+                    fuel += K
+                    cnt += 1
+                    break
 
         # 한 칸씩 앞으로 간다.
         now += 1
         # 기름도 한 칸씩 줄어든다
         fuel -= 1
+
+        if fuel == -1:
+            cnt = 0
+            break
 
 
 
